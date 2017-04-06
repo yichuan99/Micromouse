0e8db9ba66b64ebb2b361ccb7a97016ec80ca923
Yichuan Wang <wycsasa@gmail.com>
2017-04-06 11:32:29 -0400

    iinkbkbcommit:wq:wqqkb!q:qqkb:q:q!

--- PWM_control/PWM_control.ino
+++ PWM_control/PWM_control.ino
@@ -2,12 +2,13 @@
 #define motorPWM_L 3
 #define motorDIR_R 4
 #define motorPWM_R 11
-
+#define IR_1 7
 void PWM_init(){
   pinMode(motorDIR_L, OUTPUT);
   pinMode(motorPWM_L, OUTPUT);
   pinMode(motorDIR_R, OUTPUT);
   pinMode(motorPWM_R, OUTPUT);
+  
   TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS21);
   OCR2A = 0;
@@ -22,6 +23,11 @@ void WritePWM(int pwm_A, int pwm_B){
 }
 
 void MotorSpeed(int motor_L, int motor_R){
+  double l_factor = 0.96;
+  double r_factor = 1;
+
+  motor_L *= l_factor;
+  motor_R *= r_factor;
 
   motor_L *= 2.55;
   motor_R *= 2.55;
@@ -46,10 +52,21 @@ void MotorSpeed(int motor_L, int motor_R){
   WritePWM(motor_L, motor_R);
 }
 
+ int obstacle = HIGH; //HIGH indicate no obstacle detected
+
 void setup() {
   PWM_init();
+  pinMode(IR_1, INPUT);
+
 }
 
 void loop() {
-  MotorSpeed(-100, 100);
+  obstacle = digitalRead(IR_1);
+  if (obstacle == LOW){
+    MotorSpeed(-50,-50);
+    delay(1000);
+  }
+  else{
+    MotorSpeed(100, 100);
+  }
 }
